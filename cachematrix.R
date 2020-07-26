9c74ceb9c6a588267cff70eeec897d8f06d298f7
isaalvarezv <isabelalvarez_i@didiglobal.com>
2020-07-26 17:51:08 -0500

    sin corchetes

--- cachematrix.R
+++ cachematrix.R
@@ -9,9 +9,9 @@ makeCacheMatrix <- function(x = matrix()) {
     x <<- a_matrix
     m <<- NULL
   }
-  get <- function() {x}
-  setinv <- function(inv){m<<- inv} 
-  getinv <- function(){m}
+  get <- function() x
+  setinv <- function(inv) m<<- inv 
+  getinv <- function() m
   list(set = set, get = get, 
        setinv = setinv, getinv = getinv)
 }
